e915033489fec70e17834fd91def73b559553453
geowarin <geowarin@gmail.com>
2015-05-14 20:40:04 +0200

    add pointer event

--- app/src/state/Main.ts
+++ app/src/state/Main.ts
@@ -2,10 +2,17 @@ module game {
 
   export class Main extends Phaser.State {
     text:Phaser.Text;
+    nbClick:number = 0;
 
     create() {
       var thing:String = 'v3 !';
       this.text = this.add.text(10, 10, `Let's ${thing}`, {font: '65px Arial'});
+      this.input.onDown.add(this.onDown, this);
+    }
+
+    onDown(pointer:Phaser.Pointer) {
+      this.nbClick++;
+      this.text.text = `You clicked ${this.nbClick} times`;
     }
 
     update() {
